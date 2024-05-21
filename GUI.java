9eefd5166fee7e8fe808dc30fad102a45f21c9db
jupiter2143 <3279697367@qq.com>
2024-05-21 22:31:04 +0800

    test

--- GUI.java
+++ GUI.java
@@ -99,7 +99,6 @@ public class GUI {
           public void mouseReleased(MouseEvent e) {
             points.add(e.getPoint()); // 最后一个点
             label.repaint();
-            
           }
         });
 
@@ -114,47 +113,50 @@ public class GUI {
   }
 
   // 多边形不太好写，想不出来也没查出来，放一个外接矩形的，留条后路
-    private void fillPolygon(Graphics g, int[] xPoints, int[] yPoints, int nPoints) {
-      Polygon polygon = new Polygon(xPoints, yPoints, nPoints);
+  private void fillPolygon(Graphics g, int[] xPoints, int[] yPoints, int nPoints) {
+    Polygon polygon = new Polygon(xPoints, yPoints, nPoints);
 
-      // 边界矩形
-      Rectangle bounds = polygon.getBounds();
+    // 边界矩形
+    Rectangle bounds = polygon.getBounds();
 
-      // 获取多边形内部的像素点颜色
-      Image image = imageIcon.getImage();
-      BufferedImage bufferedImage = toBufferedImage(image);
+    // 获取多边形内部的像素点颜色
+    Image image = imageIcon.getImage();
+    BufferedImage bufferedImage = toBufferedImage(image);
 
-      for (int x = bounds.x; x < bounds.x + bounds.width; x++) {
-          for (int y = bounds.y; y < bounds.y + bounds.height; y++) {
-              if (polygon.contains(x, y)) {
-                  g.setColor(new Color(bufferedImage.getRGB(x, y))); // 获取像素点颜色
-                  g.fillRect(x, y, 1, 1); // 填充像素点
-                  highlight[x][y] = true; // 标记高亮像素点
-              }
-          }
+    for (int x = bounds.x; x < bounds.x + bounds.width; x++) {
+      for (int y = bounds.y; y < bounds.y + bounds.height; y++) {
+        if (polygon.contains(x, y)) {
+          g.setColor(new Color(bufferedImage.getRGB(x, y))); // 获取像素点颜色
+          g.fillRect(x, y, 1, 1); // 填充像素点
+          highlight[x][y] = true; // 标记高亮像素点
+        }
       }
+    }
   }
 
   // 更新高亮矩阵
   private void updateHighlightMatrix() {
-      for (int i = 0; i < highlight.length; i++) {
-          for (int j = 0; j < highlight[0].length; j++) {
-              highlight[i][j] = false;
-          }
+    for (int i = 0; i < highlight.length; i++) {
+      for (int j = 0; j < highlight[0].length; j++) {
+        highlight[i][j] = false;
       }
+    }
 
-      // 获取多边形内的像素点
-      for (int x = 0; x < imageIcon.getIconWidth(); x++) {
-          for (int y = 0; y < imageIcon.getIconHeight(); y++) {
-              if (label.contains(x, y)) {
-                  if (label.contains(x + 1, y + 1)) {
-                      if (highlight[x][y] && highlight[x + 1][y] && highlight[x][y + 1] &&highlight[x + 1][y + 1]) {
-                          highlight[x][y] = true;
-                      }
-                  }
-              }
+    // 获取多边形内的像素点
+    for (int x = 0; x < imageIcon.getIconWidth(); x++) {
+      for (int y = 0; y < imageIcon.getIconHeight(); y++) {
+        if (label.contains(x, y)) {
+          if (label.contains(x + 1, y + 1)) {
+            if (highlight[x][y]
+                && highlight[x + 1][y]
+                && highlight[x][y + 1]
+                && highlight[x + 1][y + 1]) {
+              highlight[x][y] = true;
+            }
           }
+        }
       }
+    }
   }
 
   private float[][] calculateEnergyWithHighlight() {
@@ -163,17 +165,17 @@ public class GUI {
     float[][] mask = new float[width][height];
 
     for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-            if (highlight[x][y]) {
-                mask[x][y] = 1e9f;
-            } else {
-                mask[x][y] = 0;
-            }
+      for (int y = 0; y < height; y++) {
+        if (highlight[x][y]) {
+          mask[x][y] = 1e9f;
+        } else {
+          mask[x][y] = 0;
         }
+      }
     }
 
     return mask;
-}
+  }
 
   // 转换Image为BufferedImage
   private BufferedImage toBufferedImage(Image image) {
@@ -365,7 +367,6 @@ public class GUI {
             hSpinner.setValue(seamCarver.height());
           }
         });
-    rightPanel.add(redoButton);
   }
 
   private void initMenuBar() {
@@ -426,7 +427,6 @@ public class GUI {
     JLabel statusBar = new JLabel("状态栏");
     statusBar.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
     frame.add(statusBar, BorderLayout.SOUTH);
-
   }
 
   private void startSelectionTool() {
