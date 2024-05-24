bbba3fe3228a355d7c4e260dc42526debb5d34c8
jupiter2143 <3279697367@qq.com>
2024-05-24 15:37:58 +0800

    test

--- Test.java
+++ Test.java
@@ -1,27 +1,6 @@
 public class Test {
-
-  private static final int XADD = 0b00;
-  private static final int YADD = 0b01;
-  private static final int XSUB = 0b11;
-  private static final int YSUB = 0b10;
-
   public static void main(String[] args) {
     GUI gui = new GUI();
     System.out.println("Hello World");
-    // SeamCarver seamCarver = new SeamCarver(new Picture("example.jpg"));
-    // seamCarver.picture().show();
-    // for (int i = 0; i < 400; i++) {
-    // seamCarver.operate(XSUB);
-    // seamCarver.operate(YSUB);
-    // seamCarver.operate(XADD);
-    // seamCarver.operate(YADD);
-    // }
-    // seamCarver.picture().show();
-    // for (int i = 0; i < 400; i++) seamCarver.operate(YADD);
-    // seamCarver.picture().show();
-    // for (int i = 0; i < 400; i++) seamCarver.undo(true);
-    // seamCarver.picture().show();
-    // for (int i = 0; i < 200; i++) seamCarver.undo(false);
-    // seamCarver.picture().show();
   }
 }
